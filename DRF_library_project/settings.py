5cd29394f6e5beb723a5021c205bc01da9f4f3aa
Roman28101 <r.kovalenko28101@gmail.com>
2023-07-28 15:57:16 +0800

    Create user app for implementaion user services

--- DRF_library_project/settings.py
+++ DRF_library_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "book_service"
+    "book_service",
+    "user"
 ]
 
 MIDDLEWARE = [
