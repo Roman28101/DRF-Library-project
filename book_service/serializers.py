6b09628a745ebf629ed0cf7d9705c9ca0f904e58
Roman28101 <r.kovalenko28101@gmail.com>
2023-07-28 15:48:15 +0800

    Add endpoints and permissions for book service

--- book_service/serializers.py
+++ book_service/serializers.py
@@ -8,3 +8,9 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "author", "cover", "inventory", "daily_fee")
+
+
+class BookCoverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "cover")
